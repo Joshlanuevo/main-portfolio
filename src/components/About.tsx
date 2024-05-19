4c947c03685372087ba070770070bda2eae893ca
Joshlanuevo <lanuevo68@gmail.com>
2024-05-19 18:45:57 +0800

    finish about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import { userData } from '../data/data';
 
 const About = () => {
   return (
-    <div  className='about' id="about">
-        <h1>About</h1>
+    <div className='about' id="about">
+      <div className="about-container">
+      <h1>About</h1>
+      <div className="underline"></div>
+      <h2>{userData.about.header}</h2>
+      <div className="content">
+        <div className="description">
+        <p dangerouslySetInnerHTML={{ __html: userData.about.description }} />
+        </div>
+        <div className="tech-stack">
+          <h3>Tech Used</h3>
+          <div className="tech-items">
+            {userData.about.tech.map((tech, index) => (
+              <div key={index} className="tech-item">
+                <img src={tech.icon} alt={tech.name} className="tech-icon" />
+                <p>{tech.name}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default About;
--- src/data/data.tsx
+++ src/data/data.tsx
@@ -67,11 +67,22 @@ export const userData = {
   address: "68 Agudo Street. Mandaluyong City",
   about: {
       header: "Welcome! I'm Josh Ivan Lanuevo, a passionate Software Developer with a strong foundation in JavaScript, React, and Kotlin.",
-      description: [
-          `My journey began as a React intern, where I honed my skills and immersed myself in the dynamic world of front-end development.`,
-          `Currently, I thrive as a Software Developer, leveraging the power of Kotlin to build robust, scalable solutions.`,
-          `Driven by curiosity and a relentless pursuit of excellence, I'm committed to crafting elegant solutions to complex problems.`,
-          `When I'm not coding, I enjoy watching anime series like My Hero Academia, Demon Slayer, Jujutsu Kaisen, and many more. Also, I enjoy playing online games with my friends, and sometimes when I get bored I sketch or draw what I like, such as anime characters or anything.`
-      ],
+      description: `My journey began as a Front End Developer intern, where I honed my skills and immersed myself in the dynamic world of Front End development. Currently, I thrive as a Software Developer, leveraging the power of Kotlin to build robust, scalable solutions. Driven by curiosity and a relentless pursuit of excellence, I'm committed to crafting elegant solutions to complex problems. 
+      <br /> 
+      <br /> 
+      When I'm not coding, I enjoy watching anime series like My Hero Academia, Demon Slayer, Jujutsu Kaisen, and many more. Also, I enjoy playing online games with my friends, and sometimes when I get bored I sketch or draw what I like, such as anime characters or anything.`,
+      tech: [
+        { name: "HTML5", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" },
+        { name: "CSS3", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" },
+        { name: "Bootstrap", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg" },
+        { name: "Tailwind CSS", icon: "https://cdn.worldvectorlogo.com/logos/tailwindcss.svg" },
+        { name: "JavaScript", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" },
+        { name: "React", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" },
+        { name: "NextJS", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg " },
+        { name: "Kotlin", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/kotlin/kotlin-original.svg" },
+        { name: "Node.js", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" },
+        { name: "MongoDB", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" },
+        { name: "Git", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" },
+      ]
   },
 }
