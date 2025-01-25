1c843bbfc4b4a793cdcfd9c199a5109438b71f56
Joshlanuevo <lanuevo68@gmail.com>
2025-01-25 21:57:48 +0800

    edit destination

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -73,7 +73,7 @@ export const social = [
 
 export const userData = {
   name: "Josh Ivan Lanuevo",
-  destination: "Front End Developer",
+  destination: "Android Developer", // "Front End Developer", 
   email: "joshlanuevo11@gmail.com",
   phone: "+63 921 457 7200",
   address: "68 Agudo Street. Mandaluyong City",
